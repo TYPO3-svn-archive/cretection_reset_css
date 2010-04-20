2514b022ddc5d0e0ba1362835eef28683f0ae9f5
cretection <cretection@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-20 08:11:51 +0000

    First Import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cretection_reset_css/tags/2.0.1@32360 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "cretection_reset_css".
+#
+# Auto generated 20-04-2010 09:26
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Reset CSS',
+	'description' => 'A Reset CSS based on Chris Meyer\'s Reset CSS.',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '2.0.0',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => 'bottom',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Jonathan Starck',
+	'author_email' => 'info@cretection.de',
+	'author_company' => 'Cretection Web- & Computertechnik',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"7ae3";s:10:"README.txt";s:4:"3924";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"0157";s:24:"ext_typoscript_setup.txt";s:4:"d001";s:13:"css/reset.css";s:4:"e760";}',
+	'suggests' => array(
+	),
+);
+
+?>
