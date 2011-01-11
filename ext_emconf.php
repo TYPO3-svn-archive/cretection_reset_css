628da9fb4f35358439ce8a1660aeb1ddfa0dd8bd
cretection <cretection@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-11 16:53:14 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cretection_reset_css/trunk@42010 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,11 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "cretection_reset_css".
-#
-# Auto generated 20-04-2010 09:26
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Reset CSS',
-	'description' => 'A Reset CSS based on Chris Meyer\'s Reset CSS.',
+	'description' => 'A Reset CSS based on Chris Meyer\'s "Reset-CSS". ',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '2.0.0',
+	'version' => '3.0.0',
 	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => 'bottom',
@@ -41,7 +31,6 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"7ae3";s:10:"README.txt";s:4:"3924";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"0157";s:24:"ext_typoscript_setup.txt";s:4:"d001";s:13:"css/reset.css";s:4:"e760";}',
 	'suggests' => array(
 	),
 );
