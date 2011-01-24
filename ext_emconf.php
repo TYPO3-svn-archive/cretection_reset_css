6a4700734f66b52b0a4c8d9142e8ce09d5d27eb8
cretection <cretection@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-24 10:04:28 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cretection_reset_css/tags/old@42518 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Reset CSS',
+	'description' => 'A Reset CSS based on Chris Meyer\'s "Reset-CSS". ',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '3.0.0',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => 'bottom',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Jonathan Starck',
+	'author_email' => 'info@cretection.de',
+	'author_company' => 'Cretection Web- & Computertechnik',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
